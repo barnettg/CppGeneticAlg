f4fe98b1234f31dfd9cd682892c015473efed3ec
Glen Barnett <glenbarnett2002@yahoo.com>
2017-06-12 21:17:30 -0700

    First Commit

---
+++ GeneticAlgorithms.cpp
@@ -0,0 +1,14 @@
+// GeneticAlgorithms.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " Hello " << endl;
+    return 0;
+}
+
